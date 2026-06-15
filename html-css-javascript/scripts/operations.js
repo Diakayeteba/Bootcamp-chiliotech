07a3d79a808b1e6526b69486f840bd80fa3b0647
Madou DIAKAYETEBA <madoudiakayeteba@gmail.com>
2026-06-15 22:42:55 +0200

    types de donnés en javascript

---
+++ html-css-javascript/scripts/operations.js
@@ -0,0 +1,17 @@
+var entier;
+entier = 10;
+
+var reel = 10.45;
+
+console.log( typeof entier, entier);
+
+let chaineDeCaracters = "Madou";
+console.log(chaineDeCaracters);
+
+const PI = 22/7;
+console.log(PI);
+
+const OTHER_PI = 3.14;
+console.log(OTHER_PI);
+const isTrainer = true;
+console.log(typeof isTrainer);
