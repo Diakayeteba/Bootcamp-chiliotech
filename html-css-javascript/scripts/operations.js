44b25fe165833647a8e3c1ab124777e7a8f6ea17
Madou DIAKAYETEBA <madoudiakayeteba@gmail.com>
2026-06-16 22:50:33 +0200

    les commentaires et les mises en forme

--- html-css-javascript/scripts/operations.js
+++ html-css-javascript/scripts/operations.js
@@ -1,14 +1,20 @@
+/*
+    formation js 
+    Presentation des types de données 
+    var(DEPRECATED)
+    
+ */
 var entier;
 entier = 10;
 
 var reel = 10.45;
 
-console.log( typeof entier, entier);
+console.log(typeof entier, entier);
 
 let chaineDeCaracters = "Madou";
-console.log(chaineDeCaracters);
+console.log(typeof chaineDeCaracters, chaineDeCaracters);
 
-const PI = 22/7;
+const PI = 22 / 7;
 console.log(PI);
 
 const OTHER_PI = 3.14;
@@ -19,4 +25,3 @@ console.log(typeof IS_TRAINER);
 
 let numbers = [10, 3, 4, 45];
 console.log(typeof numbers, numbers);
-
