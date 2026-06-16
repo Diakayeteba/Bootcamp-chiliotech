f71782b5c77581d49a519a9b691b40614a2a46a8
Madou DIAKAYETEBA <madoudiakayeteba@gmail.com>
2026-06-16 23:12:05 +0200

    liste des opérations

--- html-css-javascript/scripts/operations.js
+++ html-css-javascript/scripts/operations.js
@@ -1,27 +1,11 @@
-/*
-    formation js 
-    Presentation des types de données 
-    var(DEPRECATED)
-    
- */
-var entier;
-entier = 10;
+let resultat = 0;
+const firstNumber = 10;
+const secondNumber = 20;
 
-var reel = 10.45;
+resultat = firstNumber + secondNumber ;
 
-console.log(typeof entier, entier);
+// le resultat doit etre 30
+console.log("La somme est :", resultat);
 
-let chaineDeCaracters = "Madou";
-console.log(typeof chaineDeCaracters, chaineDeCaracters);
-
-const PI = 22 / 7;
-console.log(PI);
-
-const OTHER_PI = 3.14;
-console.log(OTHER_PI);
-
-const IS_TRAINER = true;
-console.log(typeof IS_TRAINER);
-
-let numbers = [10, 3, 4, 45];
-console.log(typeof numbers, numbers);
+resultat = firstNumber * secondNumber;
+console.log("le produit est :",resultat)
