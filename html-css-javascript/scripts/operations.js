2787e256c4a16b9c59f0b899d9595d8693660ccc
Madou DIAKAYETEBA <madoudiakayeteba@gmail.com>
2026-06-19 20:55:48 +0200

    realisation du TP 'chaine de caractere'

--- html-css-javascript/scripts/operations.js
+++ html-css-javascript/scripts/operations.js
@@ -14,15 +14,34 @@ function calculer(event) {
     console.log(valueOfsecondNumberInput);
     let result =0;
 
-    if (selectedOperation == 1){
+    console.log(typeof selectedOperation);
+
+    if (Number(selectedOperation) === 1){
         result = 1 * valueOfsecondNumberInput + 1 * valueOfFirstNumberInput;
-    } else if (selectedOperation == 2){
-          result = 1 * valueOfsecondNumberInput * 1 * valueOfFirstNumberInput;
+    } else if (Number(selectedOperation) === 2){
+          result = Number(valueOfsecondNumberInput) * Number(valueOfFirstNumberInput);
     
     }
-    console.log(result);
+    console.log( "result" + result);
+
+    const name = document.getElementById("name");
+    const valueOfNameInput = name.value;
+    console.log ('Name : $(valueOfNameInput)');
+
+    // les Initiales 
+    const firstNameAndLastname = valueOfNameInput.split("");
+    const initials = `${firstNameAndLastname[0][0]}${firstNameAndLastname[0][1]}`;
     
-    document.getElementById("result").innerHTML = result;
+
+    // concatenation
+    let nameAndResutlt = valueOfNameInput +  "" + result;
+    nameAndResutlt = `${initials.toUpperCase} ${result}`;
+    
+    // la longueur d'une chaine;
+    const length = nameAndResutlt.length;
+    console.log(length);
+
+    document.getElementById("result").innerHTML = nameAndResutlt;
 
 
 }
