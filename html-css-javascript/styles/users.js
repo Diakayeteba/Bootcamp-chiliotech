c0c83a97ca2dea7258c7e67fa5dcb84fa67a2078
Madou DIAKAYETEBA <madoudiakayeteba@gmail.com>
2026-06-25 22:19:29 +0200

    recuperation de données utilisateurs dans le tableau

--- html-css-javascript/styles/users.js
+++ html-css-javascript/styles/users.js
@@ -2,15 +2,28 @@ function handleResponse (response){
     return response.json();
 }
 
-const handleResponse = (data) => {
+const handleData = (data) => {
     console.log(data);
+    let tablelines = "";
+    for (let index = 0; index < Array.length; index++){
+        const user = data[index];
+        const tableLine = `
+        <tr>
+            <th>${user.name}</th>
+            <th>${user.phone}</th>
+            <th>${user.email}</th>
+        </tr>
+       `;
+       tableLines = tableLines + tableLine;
+
+    }
+    document.getElementById("users-body").innerHTML = tableLines;
 };
 
 function fetchUsers(){
      fetch ("https://jsonplaceholder.typicode.com/users")
      .then((response) => handleResponse(response))
-    
-     .then((data) => handleResponse(data));
+     .then((data) => handleData(data));
        // JSON -> javascript object notation
         
 
